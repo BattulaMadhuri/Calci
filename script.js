c3b41e1ce1fb7a651fa0bd088fc33cdc2d22fd04
Madhuri Battula <137879232+BattulaMadhuri@users.noreply.github.com>
2023-07-19 16:08:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function add(){
+    let a = parseInt(document.getElementById("value1").value);
+    let b = parseInt(document.getElementById("value2").value);
+    let c = a + b;
+    document.getElementById("answer1").innerHTML="Addition is " +c;
+}function sub(){
+    let a = parseInt(document.getElementById("value1").value);
+    let b = parseInt(document.getElementById("value2").value);
+    let c = a - b;
+    document.getElementById("answer2").innerHTML="Subtraction is " +c;
+}
+function mul(){
+    let a = parseInt(document.getElementById("value1").value);
+    let b = parseInt(document.getElementById("value2").value);
+    let c = a * b;
+    document.getElementById("answer3").innerHTML="Multiplction is " +c;
+}
